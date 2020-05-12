473af41910427fb4f81459b12a51a8c9a177b6f5
Ricardo Russ <ricardoruss@live.com>
2020-05-12 12:01:19 -0400

    login page front-end done

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -11,20 +11,28 @@ class Home extends Component {
 <div className="row">
     <form className="col s12 center">
       <div className="row">
-        <div className="input-field col s6">
+        <div className="input-field col s12">
           <i className="material-icons prefix">account_circle</i>
           <input id="icon_prefix" type="text" className="validate"/>
           <label for="icon_prefix">Username</label>
         </div>
         </div>
         <div className="row">
-        <div className="input-field col s6">
+        <div className="input-field col s12">
           <i className="material-icons prefix">sms</i>
           <input id="icon_sms" type="password" className="validate"/>
           <label for="icon_">Password</label>
-          </div>
+          </div>  
         </div>
     </form>
+    <div className="row">
+          <button className="btn waves-effect waves-light" id="ButtonColor" type="submit" name="action">Sign in!
+    <i className="material-icons right">send</i>
+  </button>
+   </div>
+    <div className="row">
+  <a className="waves-effect waves-light btn" id="ButtonColor" href="/createAccount"><i className="material-icons left">cloud</i>Create a new account?</a>
+ </div>
   </div>
       </div>
     );
