f493ebb922306056f68f7ae6857c60e9ffab1c25
drospond <david.rospond@gmail.com>
2020-05-12 11:22:51 -0400

    authController created by Devin

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const db = require("./models");
 const UserController = require("./controllers/userController");
 const GameController = require("./controllers/gameController")
+const AuthController = require("./controllers/authController")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,6 +21,7 @@ app.get("/api/config", (req, res) => {
 
 app.use("/api/user", UserController);
 app.use("/api/game", GameController);
+app.use("/api/auth", AuthController);
 
 // db.sequelize.sync({force: true}).then(function () {
 db.sequelize.sync().then(function () {
