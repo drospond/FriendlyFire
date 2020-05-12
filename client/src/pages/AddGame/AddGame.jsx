03a8b85204fb52e412a2f95b5a0f74c292cc53ed
Ricardo Russ <ricardoruss@live.com>
2020-05-12 14:35:18 -0400

    adding in a basic templates for add game/add friends -- need to add buttons for ease of navigation

--- client/src/pages/AddGame/AddGame.jsx
+++ client/src/pages/AddGame/AddGame.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const AddGame = () => {
     return (
-        <div>
-            Page to add games
+        <div className="container center">
+           <h3>Add a new game to your profile?</h3>
+           <form class="col s12">
+      <div class="row">
+        <div class="input-field col offset-s4 s4">
+          <input placeholder="Doom" id="add_a_game" type="text"/>
+          <label for="add_a_game">Search Game here</label>
+          <button className="btn waves-effect waves-light" id="ButtonColor" type="submit" name="action">Add Game<i className="material-icons right">search</i>
+  </button>
+        </div>
+        </div>
+        </form>
         </div>
     );
 };
--- client/src/pages/FindFriends/FindFriend.jsx
+++ client/src/pages/FindFriends/FindFriend.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 
 const FindFriend = () => {
     return (
-        <div>
-            Find a friend page
+        <div className="container center">
+            <form class="col s12">
+      <div class="row">
+          <h5 id="FFheadText">Search for a new friend!</h5>
+        <div class="input-field col s6">
+          <input placeholder="Username" id="search_by_name" type="text"/>
+          <label for="search_by_name">Search By Name</label>
+          <button className="btn waves-effect waves-light" id="ButtonColor" type="submit" name="action">Search
+    <i className="material-icons right">search</i>
+  </button>
+        </div>
+        <div class="input-field col s6">
+        <textarea id="search_by_game" class="materialize-textarea"></textarea>
+          <label for="search_by_game">Search By Game</label>
+          <button className="btn waves-effect waves-light" id="ButtonColor" type="submit" name="action">Search
+    <i className="material-icons right">search</i>
+  </button> 
+        </div>
+        </div>
+        </form>
         </div>
     );
 };
