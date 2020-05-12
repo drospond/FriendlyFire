d0e676a7fc7bf43d6f7d93ce5006e3671b85f8bc
drospond <david.rospond@gmail.com>
2020-05-11 23:07:34 -0400

    create user route added

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { Op } = require("sequelize");
+const router = express.Router();
+const db = require("../models");
+
+router.post("/", (req, res) => {
+  const email = req.body.email.trim();
+  const password = req.body.password.trim();
+  const handle = req.body.handle.trim();
+  const discord = req.body.discord.trim();
+
+  db.User.create({ email, password, handle, discord })
+    .then(() => {
+      res.json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+      res.json({
+        success: false,
+        message: "Failed to create new user.",
+      });
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const db = require("./models");
+const UserController = require("./controllers/userController");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,6 +16,8 @@ app.get("/api/config", (req, res) => {
     success: true,
   });
 });
+
+app.use("/api/user", UserController);
 
 // db.sequelize.sync({force: true}).then(function () {
 db.sequelize.sync().then(function () {
