b519b7e7f8564d05e193e3b57592a958670c2c79
drospond <david.rospond@gmail.com>
2020-05-12 14:02:09 -0400

    delete route friend api

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -3,29 +3,51 @@ const router = express.Router();
 const db = require("../models");
 
 router.post("/", (req, res) => {
-    console.log("req body: ", req.body);
-    const newFriend = {
-      user1Id: req.body.user1Id,
-      user2Id: req.body.user2Id,
-    };
-    db.Friend.create(newFriend)
-      .then(() => {
-        res.json([
-          newFriend,
-          {
-            success: true,
-          },
-        ]);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500);
-        res.json({
-          success: false,
-          message: "Failed to create friend.",
-        });
+  const newFriend = {
+    user1Id: req.body.user1Id,
+    user2Id: req.body.user2Id,
+  };
+  db.Friend.create(newFriend)
+    .then(() => {
+      res.json([
+        newFriend,
+        {
+          success: true,
+        },
+      ]);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+      res.json({
+        success: false,
+        message: "Failed to create friend.",
       });
-  });
+    });
+});
 
+router.delete("/:user1Id/:user2Id", (req, res) => {
+  db.Friend.destroy({
+      where: {
+        user1Id: req.params.user1Id,
+        user2Id: req.params.user2Id
+      }
+    })
+    .then(() => {
+      res.json(
+        {
+          success: true,
+        },
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+      res.json({
+        success: false,
+        message: "Failed to delete friend.",
+      });
+    });
+});
 
 module.exports = router;
