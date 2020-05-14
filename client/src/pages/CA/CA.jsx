06e07ac11f3a7b88e05ecc6744fef768c8af76d0
D3viii <devinsdevs@gmail.com>
2020-05-14 10:09:51 -0400

    Create Account built

--- client/src/pages/CA/CA.jsx
+++ client/src/pages/CA/CA.jsx
@@ -1,93 +1,73 @@
 import React, { Component } from "react";
 import "./CA.css";
-// import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 class CreateAccount extends Component {
- state = {
-   username: "",
-   email: "",
-   password: "",
- }
-
-//  handleChange = (event) => {
-//    const {email, value } = event.target;
-//    this.setState({
-//      [email]: value,
-//    })
-//  }
-
-
-
-// handleSubmit = (event) => {
-//   event.preventDefault();
-//   axios
-//     .put(`/api/user/${this.props.match.params.id}`, {
-//       username: this.state.username,
-//       email: this.state.email,
-//       password: this.state.password,
-      
-//     })
-//     .then((response) => {
-//       console.log(response);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-
-    
-//   };
-
+  state = {
+    email: "",
+    username: "",
+    password: "",
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .put(`/api/user/${this.props.match.params.id}`, {
+        email: this.state.email,
+        username: this.state.username,
+        password: this.state.password,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   render() {
     return (
-      <div className="container">
-      <div className="row">
-        <div className="col">
-          <h1>Create account info</h1>
+      <div className="container center">
+        <h1 id="FFheadText"> Welcome to FriendlyFire! </h1>
+        <br />
+        <div className="row">
+          <form className="col s12 center">
+            <div className="row">
+              <div className="input-field col s12">
+                <i className="material-icons prefix">account_circle</i>
+                <input id="icon_prefix" type="text" />
+                <label for="icon_prefix">Username</label>
+              </div>
+              <div className="input-field col s12">
+                <i className="material-icons prefix">account_circle</i>
+                <input id="icon_prefix" type="text" />
+                <label for="icon_prefix">Email</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <i className="material-icons prefix">sms</i>
+                <input id="icon_sms" type="password" />
+                <label for="icon_">Password</label>
+              </div>
+            </div>
+          </form>
+          <div className="row">
+            <button
+              className="btn waves-effect waves-light"
+              href="/AboutMe"
+              id="ButtonColor"
+              type="submit"
+              name="action"
+            >
+              Next!
+              <i className="material-icons right">send</i>
+            </button>
+          </div>
+        
         </div>
       </div>
-      <div className="row">
-        <form
-          className="col s12"
-          onSubmit={(e) => {
-            this.handleSubmit(e);
-          }}
-        >
-          <input
-            id="username"
-            label="Username"
-            type="text"
-            name="username"
-            value={this.state.username}
-            handleChange={this.handleChange}
-          />
-          <br/>
-          <input
-            id="email"
-            label="Email"
-            type="text"
-            name="email"
-            value={this.state.email}
-            handleChange={this.handleChange}
-          />
-           <input
-            id="password"
-            label="Password"
-            type="text"
-            name="password"
-            value={this.state.password}
-            handleChange={this.handleChange}
-          />
-        
-           
-          <button text="Submit"></button>
-        </form>
-      </div>
-    </div>
-    )
+    );
   }
 }
-
-
-  
 
 export default CreateAccount;
