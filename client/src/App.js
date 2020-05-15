f7f7e77c9e929c2438b204977b07e5cd28ad8196
drospond <david.rospond@gmail.com>
2020-05-15 10:16:48 -0400

    basic auth working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
+import jwt from "jsonwebtoken";
 import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/Persistent/Navbar";
 import Footer from "./components/Persistent/Footer"
@@ -12,6 +13,41 @@ import AddGame from './pages/AddGame/AddGame';
 import NoMatch from './pages/NoMatch/NoMatch'
 
 function App() {
+  const [userObject, setUserObject] = useState({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    checkForToken();
+  }, []);
+
+  const checkForToken = async () => {
+    const tokenFromStorage = await sessionStorage.getItem("jwt");
+    if (tokenFromStorage) {
+      setIsLoggedIn(true);
+      try {
+        const decoded = await jwt.verify(
+          tokenFromStorage,
+          process.env.REACT_APP_SECRET_KEY
+        );
+        if (decoded && decoded.email && decoded.id) {
+          setUserObject(decoded);
+          setIsLoggedIn(true);
+        }
+      } catch (e) {
+        setUserObject({});
+        setIsLoggedIn(false);
+        sessionStorage.setItem("jwt", "");
+        console.error(e);
+      }
+    }
+  };
+
+  const logOutUser = () => {
+    setUserObject({});
+    setIsLoggedIn(false);
+    sessionStorage.setItem("jwt", "");
+  };
+
   return ( 
   <Router>
     <header>
@@ -20,9 +56,9 @@ function App() {
       <div>
       <NavBar/>
     <Switch>
-      <Route exact path={["/", "/landing"]}>
-        <Home/>
-      </Route>
+      <Route exact path={["/", "/landing"]} render={(props) => (
+              <Home {...props} checkForToken={checkForToken} />
+            )}/>
       <Route exact path={["/about"]}>
       <AboutMe/>
         </Route>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,39 +1,94 @@
 import React, { Component } from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import jwt from "jsonwebtoken";
+import axios from "axios"
 
 class Home extends Component {
+  state = {
+    email: "",
+    password: "",
+    error: "",
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+      error: "",
+    });
+  };
+
+  handleSubmit = (event, email, password) => {
+    event.preventDefault();
+    axios
+      .post("/api/auth", {
+        email,
+        password,
+      })
+      .then(async (response) => {
+        console.log(response.data.data);
+        if (response.data.success) {
+          const decoded = await jwt.verify(
+            response.data.data,
+            process.env.REACT_APP_SECRET_KEY
+          );
+          console.log(decoded);
+          await sessionStorage.setItem("jwt", response.data.data);
+          await this.props.checkForToken();
+          await this.props.history.push(`/dashboard/${decoded.id}`);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ error: true });
+      });
+  };
+
   render() {
     return (
       <div className="container center">
         <h1 id="FFheadText"> Welcome to FriendlyFire! </h1>
         <br />
-<div className="row">
-    <form className="col s12 center">
-      <div className="row">
-        <div className="input-field col s12">
-          <i className="material-icons prefix">account_circle</i>
-          <input id="icon_prefix" type="text"/>
-          <label for="icon_prefix">Username</label>
-        </div>
-        </div>
         <div className="row">
-        <div className="input-field col s12">
-          <i className="material-icons prefix">sms</i>
-          <input id="icon_sms" type="password"/>
-          <label for="icon_">Password</label>
-          </div>  
+          <form className="col s12 center" onSubmit={(event)=>this.handleSubmit(event, this.state.email, this.state.password)}>
+            <div className="row">
+              <div className="input-field col s12">
+                <i className="material-icons prefix">account_circle</i>
+                <input id="icon_prefix" type="text" name="email" onChange={this.handleInputChange}/>
+                <label for="icon_prefix">Username</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <i className="material-icons prefix">sms</i>
+                <input id="icon_sms" type="password" name="password" onChange={this.handleInputChange}/>
+                <label for="icon_">Password</label>
+              </div>
+            </div>
+          <div className="row">
+            <button
+              className="btn waves-effect waves-light"
+              href="/dashboard"
+              id="ButtonColor"
+              type="submit"
+              name="action"
+            >
+              Sign in!
+              <i className="material-icons right">send</i>
+            </button>
+          </div>
+          </form>
+          <div className="row">
+            <a
+              className="waves-effect waves-light btn"
+              id="ButtonColor"
+              href="/createAccount"
+            >
+              <i className="material-icons left">cloud</i>Create a new account?
+            </a>
+          </div>
         </div>
-    </form>
-    <div className="row">
-          <button className="btn waves-effect waves-light" href="/dashboard" id="ButtonColor" type="submit" name="action">Sign in!
-    <i className="material-icons right">send</i>
-  </button>
-   </div>
-    <div className="row">
-  <a className="waves-effect waves-light btn" id="ButtonColor" href="/createAccount"><i className="material-icons left">cloud</i>Create a new account?</a>
- </div>
-  </div>
       </div>
     );
   }
