2aa713feede45823d08adc4463c41050342b5d55
drospond <david.rospond@gmail.com>
2020-05-16 14:31:39 -0400

    add friend button

--- client/src/components/friendHandler/friendList.jsx
+++ client/src/components/friendHandler/friendList.jsx
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import Friend from './friend'
+import React, { Component } from "react";
+import Friend from "./friend";
 
 const FriendList = (props) => {
-        return (
-        <tbody>
-            {props.friendResults.map((friendResults) =>( 
-                <Friend {...friendResults}
-                key ={friendResults.id} 
-              id={friendResults.id}
-              Username={friendResults.handle}
-              discordName={friendResults.discord}/>
-            ))}
-        </tbody>
-        );
-    }
-
+  return (
+    <tbody>
+      {props.friendResults.map((friendResults) => (
+        <Friend
+          {...friendResults}
+          key={friendResults.id}
+          id={friendResults.id}
+          Username={friendResults.handle}
+          discordName={friendResults.discord}
+          saveButton={props.saveButton}
+          addFriend={props.addFriend}
+        />
+      ))}
+    </tbody>
+  );
+};
 
 export default FriendList;
