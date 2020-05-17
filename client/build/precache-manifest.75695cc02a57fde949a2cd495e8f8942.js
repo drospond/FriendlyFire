57cf788d510ec4a1d8a4b02d670eb7bd96c7377b
Ricardo Russ <ricardoruss@live.com>
2020-05-17 19:05:45 -0400

    build pt3

---
+++ client/build/precache-manifest.75695cc02a57fde949a2cd495e8f8942.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54fd349a92b92c9c66a906db1b4107b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75641ff4fe2dee385939",
+    "url": "/static/css/main.9ca1c1ed.chunk.css"
+  },
+  {
+    "revision": "b11e7c26da4b0029e172",
+    "url": "/static/js/2.f14fe244.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/static/js/2.f14fe244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75641ff4fe2dee385939",
+    "url": "/static/js/main.701f143a.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
